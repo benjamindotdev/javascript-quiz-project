82542daa4255ca13a9581b801f618f5172dd7223
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-06-24 14:27:30 +0200

    Hello, world

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,39 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  correctAnswers = 0;
+  currentQuestionIndex = 0;
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 5. checkAnswer(answer)
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    this.questions.sort(() =>
+      Math.floor(Math.random() * this.questions.length)
+    );
+  }
 
-    // 6. hasEnded()
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    this.getQuestion().answer === answer ? this.correctAnswers++ : null;
+  }
+
+  // 6. hasEnded()
+  hasEnded() {
+    return this.currentQuestionIndex === this.questions.length;
+  }
 }
