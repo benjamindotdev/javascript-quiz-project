34409d8dd144adae0ba150954cf545440420af62
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-06-25 15:37:54 +0200

    Benjamin Elliott

--- src/quiz.js
+++ src/quiz.js
@@ -35,4 +35,65 @@ class Quiz {
   hasEnded() {
     return this.currentQuestionIndex === this.questions.length;
   }
+
+  filterQuestionsByDifficulty(difficulty) {
+    // return difficulty > 0 && difficulty <= 3
+    //   ? this.questions.filter((question) => {
+    //       return question.difficulty === difficulty;
+    //     })
+    //   : "Invalid difficulty level";
+    return this.questions.filter(
+      (question) => question.difficulty == difficulty
+    );
+  }
+
+  averageDifficulty() {
+    return (
+      this.questions
+        .reduce((acc, question) => acc + question.difficulty, 0)
+        .toFixed(1) / this.questions.length
+    );
+  }
 }
+
+const questions = [
+  {
+    text: "Question 1",
+    choices: ["a", "b", "c"],
+    answer: "a",
+    difficulty: 1,
+  },
+  {
+    text: "Question 2",
+    choices: ["d", "e", "f"],
+    answer: "d",
+    difficulty: 2,
+  },
+  {
+    text: "Question 2",
+    choices: ["d", "e", "f"],
+    answer: "d",
+    difficulty: 2,
+  },
+  {
+    text: "Question 2",
+    choices: ["d", "e", "f"],
+    answer: "d",
+    difficulty: 2,
+  },
+  {
+    text: "Question 3",
+    choices: ["g", "h", "i"],
+    answer: "g",
+    difficulty: 3,
+  },
+];
+
+// Create a new Quiz instance object
+const quiz = new Quiz(questions, 60, 100);
+
+console.log(quiz);
+quiz.filterQuestionsByDifficulty(1);
+console.log(quiz);
+
+console.log(quiz.filterQuestionsByDifficulty(1));
