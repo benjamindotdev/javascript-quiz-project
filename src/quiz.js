c6ed30ba6f266d8e596ec7922f8640aea7ea71ec
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-06-26 15:59:52 +0200

    day 3

--- src/quiz.js
+++ src/quiz.js
@@ -38,9 +38,9 @@ class Quiz {
 
   filterQuestionsByDifficulty(difficulty) {
     difficulty > 0 && difficulty <= 3
-      ? (this.questions = this.questions.filter((question) => {
-          return question.difficulty === difficulty;
-        }))
+      ? (this.questions = this.questions.filter(
+          (question) => question.difficulty === difficulty
+        ))
       : "Invalid difficulty level";
   }
 
